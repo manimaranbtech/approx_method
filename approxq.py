66cc843edb0b161a8f65022b26cf9328f656c960
manimaranbtech <86833925+manimaranbtech@users.noreply.github.com>
2025-06-23 14:03:15 +0530

    Create approxq.py

---
+++ approxq.py
@@ -0,0 +1,28 @@
+from sympy import symbols, Eq, solve, simplify
+
+# Define symbols
+a0, a1, x, x1, x2, u1, u2, L = symbols('a0 a1 x x1 x2 u1 u2 L')
+
+# Define equations
+eq1 = Eq(u1, a0 + a1*x1)
+eq2 = Eq(u2, a0 + a1*x2)
+
+# Solve for a0 and a1
+solution = solve((eq1, eq2), (a0, a1))
+
+# Substitute L = x2 - x1 into the solutions
+a0_sol = solution[a0].subs(x2 - x1, L)
+a1_sol = solution[a1].subs(x2 - x1, L)
+
+# General expression for u(x)
+u_x = a0_sol + a1_sol * x
+
+# Factor out u1 and u2
+expanded_num = u_x.expand()  # Expand the numerator
+factored_u_x = expanded_num.collect([u1, u2])  # Group terms with u1 and u2
+
+
+print(u_x)
+print(a0_sol)
+print(a1_sol)
+print(factored_u_x)
